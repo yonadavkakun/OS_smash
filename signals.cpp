ec56ab878e67342a1ad5b9bfe6c10f7cb4481565
yonadavkakun <yonadav4321@gmail.com>
2025-04-29 11:02:49 +0300

    imp ctrlC handler

--- signals.cpp
+++ signals.cpp
@@ -6,5 +6,14 @@
 using namespace std;
 
 void ctrlCHandler(int sig_num) {
+    SmallShell &smash = SmallShell::getInstance();
+    if (smash.getFgProcPID() > 0) {
+        if (kill(smash.getFgProcPID(), SIGINT) == -1) {
+            perror("smash error: kill failed");
+        } else {
+            std::cout << "smash: process " << smash.getFgProcPID() << " was killed" << std::endl;
+            smash.clearFgJob();
+        }
+    }
     // TODO: Add your implementation
 }
